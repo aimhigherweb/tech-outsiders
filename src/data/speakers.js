c7220f4dfd463a555614dd0ea04b26e04c51ac9f
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-08-23 10:47:13 +0800

    updated my speaker details

--- src/data/speakers.js
+++ src/data/speakers.js
@@ -38,7 +38,8 @@ const speakers = [
         tagline: 'Front End Jedi, Tech Evangelist, Dog Lover',
         bio: (
             <Fragment>
-                <p>Amy is a freelance developer, <a href="https://aimhigherwebdesign.com.au/" target="_blank">starting her own business</a>, working with other freelancers and learning everything she can about the web. She’s been a freelancer on the side for 4 years, and most recently worked at an agency in Subiaco.</p><p>Amy also spends her time helping out as a co-organiser for <a href="https://fenders.co/" target="_blank">Fenders</a> (a community group of front-end developers in Perth), other community events (such as Perth Web Girls) and works as an evangelist for YOW! Conference. She <a href="https://amygoestoperth.com.au/" target="_blank">blogs in her spare time</a> (about tech, the web and life), and volunteers as a reviewer for the Fringe World Festival in Perth. Amy has also started <a href="https://thefreelance.guide/" target="_blank">The Freelance Guide</a> to help other freelancers.</p>
+                <p>Amy is a freelance developer, <a href="https://aimhigherwebdesign.com.au/" target="_blank">starting her own business</a>, working with other freelancers and learning everything she can about the web. She’s been a freelancer on the side for 4 years, and most recently worked at an agency in Subiaco.</p>
+                <p>Amy also spends her time helping out as a co-organiser for <a href="https://fenders.co/" target="_blank">Fenders</a> (a community group of front-end developers in Perth), other community events (such as Perth Web Girls) and works as an evangelist for YOW! Conference. She <a href="https://amygoestoperth.com.au/" target="_blank">blogs in her spare time</a> (about tech, the web and life), and volunteers as a reviewer for the Fringe World Festival in Perth. Amy has also started <a href="https://thefreelance.guide/" target="_blank">The Freelance Guide</a> to help other freelancers.</p>
             </Fragment>
         ),
         mentoring: (
@@ -55,18 +56,24 @@ const speakers = [
             },
             codepen: 'https://codepen.io/aimhigherwebdesign-amy/',
             facebook: 'https://www.facebook.com/aimhigherwebdesign',
-            github: 'https://github.com/amykapernick/amygoestoperth',
+            github: 'https://github.com/amykapernick',
             instagram: 'https://www.instagram.com/amys_kapers/',
             linkedin: 'https://www.linkedin.com/in/amykapernick/',
             twitter: 'https://twitter.com/amys_kapers',
             website: 'https://aimhigherweb.design',
             featured: [
-                'blog2',
-                'github',
+                'blog1',
+                'website',
                 'twitter'
             ],
         },
         talks: [
+            {
+                title: 'The Art of Failure',
+                date: '07 Aug 2018',
+                event: 'ACS Women - Ignite Talks',
+                eventLink: 'https://www.acs.org.au/cpd-education/event-detail.html?eventId=70190000001tRXfAAM',
+            },
             {
                 title: 'So you think you know HTML?',
                 date: '20 Jun 2018',
