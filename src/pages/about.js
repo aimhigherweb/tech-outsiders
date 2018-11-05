7a7ae181ce97e415d43493bf3c83383b95001cba
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-11-05 15:05:40 +0800

    added info about publishing to site

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,6 +16,10 @@ const About = () => (
 		<p>Tech Outsiders is a social enterprise based in WA that is raising awareness of all the cool people we have working in tech who are outside the industry stereotype of the "straight white male".</p>
 		<p>The most common response we hear to the complaint "All the speakers at the conference are men!" or "All the award nominees are male" is that there were "No known female candidates".</p>
 		<blockquote>It is our mission to solve that problem by highlighting all of the marvellous talent the tech industry has outside the default</blockquote>
+		<h2>How can I get on Tech Outsiders?</h2>
+		<p>If you or someone you know should be on Tech Outsiders, create an account at login to <a href="https://techoutsiders.com.au/admin/" target="_blank">techoutsiders.com.au/admin</a> and create a new speaker profile. You can view the profile straight away (give or take a couple of minutes) on <a href="https://dev.techoutsiders.com.au/" target="_blank">dev.techoutsiders.com.au</a> and once the Tech Outsiders team has confirmed everything is accurate it'll appear on the live site as well.</p>
+		<h2>How can I update my Tech Outsiders profile?</h2>
+		<p>This is the same as creating a new profile. Just login to <a href="https://techoutsiders.com.au/admin/" target="_blank">techoutsiders.com.au/admin</a> and edit your speaker profile. We'll check the information again and make them available on the live site.</p>
 	</Layout>
 )
 
