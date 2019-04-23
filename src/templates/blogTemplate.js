f35b80d459ef482f2e58b7b4e800376001f0e4cb
Amy Kapernick <amy@aimhigherwebdesign.com>
2019-04-23 12:30:59 +0800

    removed blog post query

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -5,7 +5,7 @@ import Img from 'gatsby-image'
 import Layout from '../components/layout'
 
 const BlogPost = ({ data }) => {
-	const post = data
+	// const post = data
 
 	const meta = {
 		// 	name: profile.frontmatter.title + ' | ' + site.siteMetadata.title,
@@ -22,26 +22,26 @@ const BlogPost = ({ data }) => {
 
 export default BlogPost
 
-export const pageQuery = graphql`
-	query BlogPostID($id: String!) {
-		site {
-			siteMetadata {
-				title
-				siteUrl
-			}
-		}
-		markdownRemark(id: { eq: $id }) {
-			id
-			fields {
-				slug
-			}
-			html
-			frontmatter {
-				publishDate(formatString: "DD MMM YYYY")
-				title
-				description
-				tags
-			}
-		}
-	}
+// export const pageQuery = graphql`
+// 	query BlogPostID($id: String!) {
+// 		site {
+// 			siteMetadata {
+// 				title
+// 				siteUrl
+// 			}
+// 		}
+// 		markdownRemark(id: { eq: $id }) {
+// 			id
+// 			fields {
+// 				slug
+// 			}
+// 			html
+// 			frontmatter {
+// 				publishDate(formatString: "DD MMM YYYY")
+// 				title
+// 				description
+// 				tags
+// 			}
+// 		}
+// 	}
 `
